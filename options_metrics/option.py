d82e1d2d5ec26eddcc6a8fd8f15a68c73f5ad096
Mathïs Fédérico <mathfederico@gmail.com>
2021-07-09 20:34:48 +0200

    Updated Option with cached graph

--- options_metrics/option.py
+++ options_metrics/option.py
@@ -1,11 +1,13 @@
 import networkx as nx
 
+
 class Option():
 
     """ Abstract class for options """
 
     def __init__(self, option_id) -> None:
         self.option_id = option_id
+        self._graph = None
 
     def __call__(self, observations, greedy: bool=False):
         """ Use the option to get next actions.
@@ -24,5 +26,15 @@ class Option():
     def interest(self, observations) -> float:
         raise NotImplementedError
 
-    def get_graph(self) -> nx.DiGraph:
+    def build_graph(self) -> nx.DiGraph:
         raise NotImplementedError
+    
+    def draw_graph(self, ax):
+        raise NotImplementedError
+
+    @property
+    def graph(self) -> nx.DiGraph:
+        if self._graph is None:
+            self._graph = self.build_graph()
+        return self._graph
+
